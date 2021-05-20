71c1e04fd78ea7298e49e7b43d45b81b4f8a8c7d
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2021-05-21 08:05:26 +0900

    feat: refactor to addEventListener

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const saveLead = () => {
   alert("Button clicked!");
 };
+
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", () => {
+  alert("Button clicked from addEventListener");
+});
