8328f9da9719ffb133fb8afeba2cd81d4b769359
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2021-05-22 07:14:18 +0900

    feat: Delete button

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 });
 
-const renderLeads = () => {
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
-      <li>
-        <a target='_blank' href='${myLeads[i]}'>
-          ${myLeads[i]}
-        </a>
-      </li>
-    `;
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
   }
-
   ulEl.innerHTML = listItems;
-};
+}
